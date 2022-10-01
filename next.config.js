250ddb21b39125caa98f596d5d941b96989ebda3
Matheus Castro do Nascimento <46095722+matheus7c@users.noreply.github.com>
2022-10-01 18:50:39 -0300

    teste de push

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
 
-module.exports = nextConfig
+module.exports = {
+    trailingSlash: true,
+    async redirects() {
+        return [
+            {
+                source: '/perguntas',
+                destination: '/faq/',
+                permanent: true,
+            },
+        ]
+    },
+}
--- src/components/patterns/Footer/index.js
+++ src/components/patterns/Footer/index.js
@@ -1,24 +1,24 @@
-import { theme } from "../../../theme/theme";
-import { Box, Text } from "../../../theme/components";
+import { theme } from '../../../theme/theme';
+import { Box, Text } from '../../../theme/components';
 
 export default function Footer() {
   return (
     <Box
       as="footer"
       styleSheet={{
-        backgroundColor: theme.colors.neutral[900],
+        backgroundColor: theme.colors.neutral[900]
       }}
     >
       <Box
         styleSheet={{
-          overflow: "hidden",
+          overflow:"hidden",
           maxWidth: theme.space.xcontainer_xl,
           marginLeft: "auto",
-          marginRight: "auto",
-          paddingVertical: {
+          marginRight:"auto",
+          paddingVertical:{
             xs: theme.space.x12,
           },
-          paddingHorizontal: {
+          paddingHorizontal:{
             xs: theme.space.x4,
             sm: theme.space.x6,
             lg: theme.space.x8,
@@ -33,10 +33,9 @@ export default function Footer() {
             color: theme.colors.neutral[400],
           }}
         >
-          &copy; {new Date().getFullYear()} DevSoutinho. Todos os direitos
-          reservados.
+          &copy; {new Date().getFullYear()} DevSoutinho. Todos os direitos reservados.
         </Text>
       </Box>
     </Box>
-  );
+  )
 }
